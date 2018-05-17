553e5c4b4fab0f0e55769e68451b6113c44a2d2e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-17 16:57:17 -0700

    HangMan IP, working on GUI

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -5,22 +7,29 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class Hangman {
+public class Hangman implements KeyListener{
 	ArrayList<String> words = new ArrayList<String>();
-	// Stack<String> hangWords = new Stack<String>();
+	Stack<String> hangWords = new Stack<String>();
 	ArrayList<Integer> ranInts = new ArrayList<Integer>();
 	Random ran = new Random();
 	int counter = 0;
 	int wanted;
+	String currentKey;
 
 	public static void main(String[] args) {
 		String am = JOptionPane.showInputDialog("How may times do you want to play?");
 		int amount = Integer.parseInt(am);
 		Hangman man = new Hangman();
 		man.numbers(amount);
+		System.out.println();
 		man.randomWord();
+		System.out.println();
+		man.GUI();
 	}
 
 	void numbers(int amount) {
@@ -38,12 +47,14 @@ public class Hangman {
 				}
 			}
 		}
+		for (Integer integer : ranInts) {
+			System.out.println(integer);
+		}
 	}
 
 	void randomWord() {
 		if (!ranInts.isEmpty()) {
 			wanted = ranInts.get(0);
-
 			try {
 				BufferedReader read = new BufferedReader(new FileReader("src/dictionary.txt"));
 				String line = read.readLine();
@@ -57,23 +68,69 @@ public class Hangman {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			for (String i : words) {
-				System.out.println(i);
+			for (String t : words) {
+				System.out.println(t);
 			}
+			randomString(words);
+		}
+	}
+
+	void randomString(ArrayList list) {
+		while(!list.isEmpty()) {
+			int random = ran.nextInt(list.size());
+			hangWords.push(words.get(random));
+			list.remove(random);
+		}
+		System.out.println();
+		for (String string : hangWords) {
+			System.out.println(string);
 		}
 	}
 
 	void checkWantedWord(String line) {
 		if (wanted == counter) {
 			words.add(line);
+			if (!ranInts.isEmpty()) {
+				ranInts.remove(0);
+			}
+
+			if (!ranInts.isEmpty()) {
+				wanted = ranInts.get(0);
+			}
 		}
 		counter++;
-		if (!ranInts.isEmpty()) {
-			ranInts.remove(0);
-		}
+	}
+	void GUI() {
+		String word = hangWords.pop();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(word);
+		frame.setVisible(true);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panel.add(label);
+		frame.pack();
+	
+	}
+	void checkKey(String word, String CurrentKey) {
+		
+	}
 
-		if (!ranInts.isEmpty()) {
-			wanted = ranInts.get(0);
-		}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		currentKey = "" + arg0.getKeyCode();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
